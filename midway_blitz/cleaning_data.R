d8cb4600b3f9602db47756a17930f5c2fba410b9
mruiz-house <mruizhouse@gmail.com>
2025-09-18 23:00:57 +0100

    Adding columns

--- midway_blitz/cleaning_data.R
+++ midway_blitz/cleaning_data.R
@@ -1,7 +1,20 @@
 # Load libraries and read data
 library(tidyverse)
-data_midway <- read_csv("midway_blitz/operation_midway_blitz.csv")
+data_midway <- read_csv('midway_blitz/operation_midway_blitz.csv',
+                        # add appropriate column names
+                        col_names = c('location', 'date_time_reported',
+                                      'address', 'date_time_raid',
+                                      'day', 'time_category', 
+                                      'raid_calls', 'notes',
+                                      'street_address', 'type_report',
+                                      'detention_centers', 'tactics_reported',
+                                      'rapid_response_team', 'people_detained',
+                                      'verified_by_rrt', 'OCAD_operator_uploaded',
+                                      'business_worksite', 'license_plate'))
 
-# Select all raids from 9/9 onward that are proven
-data_midway <- data_midway %>%
+
+# Clean the data 
+
+
+  
               
