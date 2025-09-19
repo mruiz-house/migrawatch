95bd6c9e481a2c600647908f42dcc000c7d5ca47
mruiz-house <mruizhouse@gmail.com>
2025-09-19 20:52:05 +0100

    Minimal cleaning!

--- midway_blitz/cleaning_data.R
+++ midway_blitz/cleaning_data.R
@@ -1,5 +1,7 @@
 # Load libraries and read data
 library(tidyverse)
+library(stringr)
+library(lubridate)
 data_midway <- read_csv('midway_blitz/operation_midway_blitz.csv',
                         # add appropriate column names
                         col_names = c('location', 'date_time_reported',
@@ -13,7 +15,14 @@ data_midway <- read_csv('midway_blitz/operation_midway_blitz.csv',
                                       'business_worksite', 'license_plate'))
 
 
-# Clean the data 
+# Format the date and time correctly + make certain columns strings 
+data_midway$date_time_reported <-  mdy_hm(data_midway$date_time_reported)
+data_midway$date_time_raid <-  mdy_hm(data_midway$date_time_raid)
+
+# Select all dates after 9/9 
+data_filtered <- data_midway %>%
+              filter(date_time_raid > ymd('2025-09-08')) %>%
+              filter(verified_by_rrt == 'checked')
 
 
   
