908bd6a4ac9413dc920be30b3df2c730d4c77a0a
mruiz-house <mruizhouse@gmail.com>
2026-01-10 23:00:12 -0700

    saving this ...

--- dashboard_scripts/cleaning_geolocation.R
+++ dashboard_scripts/cleaning_geolocation.R
@@ -1,45 +1,43 @@
 library(tidyverse)
 library(stringr)
 # Make sure to quote paths!
-addresses <- read_csv("dashboard_scripts/Updated_street_addresses.csv")
+addresses <- read_csv("dashboard_scripts/Updated_street_addresses.csv") %>%
+  filter(`Number of People Detained` >=1 ) %>%
+  rename(street = `Street Address or Intersection`) %>%
+  mutate(City = "",
+         State = "")
 
 # Transfer over cities and states to new column
 clean_addresses <- addresses %>%
   # separate the city names 
-  mutate(City = case_when(str_detect(`Street Address`, "48th and Rockwell|1352 32nd St|Chicago") ~ "Chicago", TRUE ~ City),
-         City = case_when(str_detect(`Street Address`, "Skokie") ~ "Skokie", TRUE ~ City),
-         City = case_when(str_detect(`Street Address`, "Evanston") ~ "Evanston", TRUE ~ City),
-         City = case_when(str_detect(`Street Address`, "Broadview") ~ "Broadview", TRUE ~ City),
-         City = case_when(str_detect(`Street Address`, "Calumet Park") ~ "Calumet Park", TRUE ~ City),
-         City = case_when(str_detect(`Street Address`, "Blue Island") ~ "Blue Island", TRUE ~ City),
-         City = case_when(str_detect(`Street Address`, "Rolling Meadows") ~ "Rolling Meadows", TRUE ~ City),
-         City = case_when(str_detect(`Street Address`, "East Dundee") ~ "East Dundee", TRUE ~ City),
-         City = case_when(str_detect(`Street Address`, "Oak Park") ~ "Oak Park", TRUE ~ City),
-         City = case_when(str_detect(`Street Address`, "Aurora") ~ "Aurora", TRUE ~ City),
-         City = case_when(str_detect(`Street Address`, "Elgin") ~ "Elgin", TRUE ~ City),
-         City = case_when(str_detect(`Street Address`, "Cicero") ~ "Cicero", TRUE ~ City),
-         City = case_when(str_detect(`Street Address`, "Joliet") ~ "Joliet", TRUE ~ City),
-         City = case_when(str_detect(`Street Address`, "Hoffman Estates") ~ "Hoffman Estates", TRUE ~ City),
+  mutate(City = case_when(str_detect(street, "Chicago") ~ "Chicago", TRUE ~ City),
+         City = case_when(str_detect(`Neighborhood or City`, "Chicago") ~ "Chicago", TRUE ~ City),
+         City = case_when(str_detect(`Neighborhood or City`, "Skokie") ~ "Skokie", TRUE ~ City),
+         City = case_when(str_detect(`Neighborhood or City`, "Evanston") ~ "Evanston", TRUE ~ City),
+         City = case_when(str_detect(`Neighborhood or City`, "Broadview") ~ "Broadview", TRUE ~ City),
+         City = case_when(str_detect(`Neighborhood or City`, "Calumet Park") ~ "Calumet Park", TRUE ~ City),
+         City = case_when(str_detect(`Neighborhood or City`, "Blue Island") ~ "Blue Island", TRUE ~ City),
+         City = case_when(str_detect(`Neighborhood or City`, "Rolling Meadows") ~ "Rolling Meadows", TRUE ~ City),
+         City = case_when(str_detect(`Neighborhood or City`, "East Dundee") ~ "East Dundee", TRUE ~ City),
+         City = case_when(str_detect(`Neighborhood or City`, "Oak Park") ~ "Oak Park", TRUE ~ City),
+         City = case_when(str_detect(`Neighborhood or City`, "Aurora") ~ "Aurora", TRUE ~ City),
+         City = case_when(str_detect(`Neighborhood or City`, "Elgin") ~ "Elgin", TRUE ~ City),
+         City = case_when(str_detect(`Neighborhood or City`, "Cicero") ~ "Cicero", TRUE ~ City),
+         City = case_when(str_detect(`Neighborhood or City`, "Joliet") ~ "Joliet", TRUE ~ City),
+         City = case_when(str_detect(`Neighborhood or City`, "Hoffman Estates") ~ "Hoffman Estates", TRUE ~ City),
   # separate the state 
-         State = case_when(str_detect(`Street Address`, "48th and Rockwell|1352 32nd St|Chicago|Elgin|Cicero") ~ "IL", TRUE ~ State),
-         State = case_when(str_detect(`Street Address`, "IL") ~ "IL", TRUE ~ State)) %>%
-  # delete zip codes, city, and state from the street addres 
-  mutate(`Street Address` = str_remove_all(`Street Address`, "\\d{5}"),
-         `Street Address` = str_remove_all(`Street Address`, "Hoffman Estates|Joliet|Chicago|Broadview|Evanston|Skokie|Cicero|Elgin|Calumet Park|Aurora|Oak Park|Rolling Meadows|East Dundee"),
-         `Street Address` = str_remove_all(`Street Address`, "IL|Il"),
-         `Street Address` = str_remove_all(`Street Address`, "[:punct:]"),
-         `Street Address` = str_remove_all(`Street Address`, "USA|United States"))
-
-# Some additional manual cleaning: 
-clean_addresses <- clean_addresses %>% 
-  mutate(`Street Address` = 
-           case_when(`Street Address` == "2219 S St Louis  " ~ "2219 South Saint Louis Street", TRUE ~ `Street Address`),
-           case_when(`Street Address` == "Esperanza Health centers on 21st and California " ~ "2001 S California Ave #100", TRUE ~ `Street Address`)) 
-
+         State = case_when(str_detect(`Neighborhood or City`, "Chicago|Elgin|Cicero") ~ "IL", TRUE ~ State),
+         State = case_when(str_detect(street, "Chicago|Elgin|Cicero") ~ "IL", TRUE ~ State)) %>%
+  # delete zip codes, city, and state from the street address
+  mutate(street = str_remove_all(street, "\\d{5}"),
+         street = str_remove_all(street, "Hoffman Estates|Joliet|Chicago|Broadview|Evanston|Skokie|Cicero|Elgin|Calumet Park|Aurora|Oak Park|Rolling Meadows|East Dundee"),
+         street = str_remove_all(street, "IL|Il"),
+         street = str_remove_all(street, "[:punct:]"),
+         street = str_remove_all(street, "USA|United States"))
 	
 
 # String Squish! 
-clean_addresses$`Street Address` <- str_squish(clean_addresses$`Street Address`) 
+clean_addresses$street <- str_squish(clean_addresses$street) 
 
 #write our beautiful csv 
 write_csv(clean_addresses, "cleaned_addresses.csv")
